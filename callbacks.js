5923df306ceb837b0cea16ee867748a7f9db0733
sheldmel <71276475+sheldmel@users.noreply.github.com>
2020-10-08 20:28:49 -0400

    Add files via upload

---
+++ callbacks.js
@@ -0,0 +1,27 @@
+function resolvedPromise() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({'message': 'delayed success!'});
+      }, 500);
+    });
+  }
+
+function rejectedPromise() {
+    return new Promise(reject => {
+      setTimeout(() => {
+        reject(Error('error: delayed exception'));
+      }, 500);
+    });
+  }
+
+  async function asyncCall1() {
+    const result = await resolvedPromise();
+    console.log(result);
+  }
+  async function asyncCall2() {
+    const result = await rejectedPromise();
+    console.log(result);
+  }
+  
+  asyncCall1()
+  asyncCall2()
